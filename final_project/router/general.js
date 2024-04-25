5b0bfd2f98c6e0b716379a1bbed074b0adcb9cdf
MarioNegoita <marionegoita@gmail.com>
2024-04-25 18:31:58 +0300

    get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,14 +24,30 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let author = req.params.author;
+  let filteredBooks = [];
+
+  for (const i in books) {
+    if (books[i].author === author) {
+      filteredBooks.push(books[i]);
+    }
+  }
+
+  res.send(filteredBooks);
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let title = req.params.title;
+  let filteredBooks = [];
+
+  for (const i in books) {
+    if (books[i].title === title) {
+      filteredBooks.push(books[i]);
+    }
+  }
+
+  res.send(filteredBooks);
 });
 
 //  Get book review
