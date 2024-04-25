fa1f301d5a8d5408d3d0ef2b2781f023649bdc3f
MarioNegoita <marionegoita@gmail.com>
2024-04-25 18:46:11 +0300

    register user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,9 +4,27 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+  let usersWithSameName = users.filter((user) => {
+    return user.username === username;
+  });
+  if (usersWithSameName.length > 0) {
+    return true;
+  }
+  return false;
+};
+
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({ username: username, password: password });
+      return res.status(200).json({ message: "User successfully registered" });
+    } else return res.status(404).json({ message: "User already exists" });
+  }
+  return res.status(404).json({ message: "Unable to register user" });
 });
 
 // Get the book list available in the shop
@@ -52,8 +70,10 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    res.send(books[isbn].review);
+  } else res.send("No book with this isbn");
 });
 
 module.exports.general = public_users;
